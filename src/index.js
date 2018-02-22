03ec38e3531f3661607a0c00c2ed6ec82de9b743
a_dzesh <krug@tut.by>
2018-02-22 10:24:20 +0300

    add function calculates triangles counts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(preferences = [ ]) {
+  var count = 0;
+  for (var i = 0; i < preferences.length; i++) {
+    if (preferences[i] !== 0) {
+      let znachA = preferences[i];
+      let znachB = preferences[znachA-1];
+      if (znachA !== znachB) {
+        let znachC = preferences[znachB-1];
+        if (znachC === preferences.indexOf(znachA)+1) {
+          count++;
+          preferences[preferences.splice(znachA-1,1,0)];
+          preferences[preferences.splice(znachB-1,1,0)];
+          preferences[preferences.splice(znachC-1,1,0)];
+        }
+      }
+    }
+  }
+return count;
 };
